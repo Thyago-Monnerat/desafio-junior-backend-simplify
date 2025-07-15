96d79182e34429961949798e8e917ebc86751ba5
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-07-15 20:07:44 -0300

    feat: add task model

---
+++ src/main/java/org/thyagomonnerat/desafiojunior/models/TaskModel.java
@@ -0,0 +1,23 @@
+package org.thyagomonnerat.desafiojunior.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String description;
+    private boolean done;
+    private int priority;
+}
