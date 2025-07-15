8ee9673ccb7919bfa952a69b887cbfbbb45c85ee
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-07-15 19:58:04 -0300

    first commit

---
+++ src/main/java/org/thyagomonnerat/desafiojunior/DesafiojuniorApplication.java
@@ -0,0 +1,13 @@
+package org.thyagomonnerat.desafiojunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiojuniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafiojuniorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/thyagomonnerat/desafiojunior/DesafiojuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thyagomonnerat.desafiojunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiojuniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
