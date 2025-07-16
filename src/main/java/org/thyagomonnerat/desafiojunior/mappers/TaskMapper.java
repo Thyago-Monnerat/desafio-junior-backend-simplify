aec4229d01c42660cbe025a60eeff0cbe747d9c7
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-07-16 17:10:49 -0300

    feat: @mapping annotation in Task mapper to ignore id field in updates

--- src/main/java/org/thyagomonnerat/desafiojunior/mappers/TaskMapper.java
+++ src/main/java/org/thyagomonnerat/desafiojunior/mappers/TaskMapper.java
@@ -1,6 +1,7 @@
 package org.thyagomonnerat.desafiojunior.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.thyagomonnerat.desafiojunior.dtos.TaskDto;
 import org.thyagomonnerat.desafiojunior.models.TaskModel;
@@ -10,5 +11,6 @@ public interface TaskMapper {
     TaskModel fromDtoToModel(TaskDto dto);
     TaskDto fromModelToDto(TaskModel model);
 
+    @Mapping(target = "id", ignore = true)
     void updateModelFromDto(TaskDto dto, @MappingTarget TaskModel model);
 }
