8ceeafe09dbede06f50667e46e1e222691e4f226
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-07-17 19:06:35 -0300

    feat: add findByName method to TaskRepository

--- src/main/java/org/thyagomonnerat/desafiojunior/repositories/TaskRepository.java
+++ src/main/java/org/thyagomonnerat/desafiojunior/repositories/TaskRepository.java
@@ -3,5 +3,8 @@ package org.thyagomonnerat.desafiojunior.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.thyagomonnerat.desafiojunior.models.TaskModel;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+    Optional<TaskModel> findByName(String name);
 }
