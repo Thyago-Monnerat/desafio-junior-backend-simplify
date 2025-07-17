481f7d4f8c7ff923318e20b8ae24ec0ef523b163
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-07-17 16:34:10 -0300

    feat: add GlobalExceptionsHandler

---
+++ src/main/java/org/thyagomonnerat/desafiojunior/exceptions/GlobalExceptionsHandler.java
@@ -0,0 +1,15 @@
+package org.thyagomonnerat.desafiojunior.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionsHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
