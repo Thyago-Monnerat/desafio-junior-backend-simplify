becead1fd5642e5a84e718e3b8f7707b78af9beb
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-07-18 15:55:24 -0300

    refactor: remove all try/catch blocks from service, leaving this responsibility to GlobalExceptionsHandler

--- src/main/java/org/thyagomonnerat/desafiojunior/services/TaskService.java
+++ src/main/java/org/thyagomonnerat/desafiojunior/services/TaskService.java
@@ -24,18 +24,13 @@ public class TaskService {
     protected TaskModel getTask(long id) {
         log.info("Attempting to get the task with the id: {}", id);
 
-        try {
-            TaskModel task = this.repository.findById(id).orElseThrow(() -> new TaskNotFoundException("Task not found"));
-            log.info("Got the task with id {} successfully", id);
-
-            return task;
-        } catch (TaskNotFoundException e) {
+        TaskModel task = this.repository.findById(id).orElseThrow(() -> {
             log.warn("Attempted to get an inexistent task by id. The id was {}", id);
-            throw e;
-        } catch (Exception e) {
-            log.error("Error to get task by id: {};", id, e);
-            throw new RuntimeException("Couldn't get the task with the id " + id, e);
-        }
+            return new TaskNotFoundException("Task not found");
+        });
+        log.info("Got the task with id {} successfully", id);
+
+        return task;
     }
 
     @Transactional(readOnly = true)
@@ -43,7 +38,7 @@ public class TaskService {
         log.info("Checking duplicate task names. Name: {}", name);
 
         this.repository.findByName(name).ifPresent((task) -> {
-            if(id == null || !id.equals(task.getId())){
+            if (id == null || !id.equals(task.getId())) {
                 log.warn("The task with the name {} already exists.", task.getName());
                 throw new TaskAlreadyExistsException("Task name already exists");
             }
@@ -60,54 +55,39 @@ public class TaskService {
     @Transactional
     public TaskDto addTask(TaskDto dto) {
         log.info("Attempting to create new task");
+        checkDupeTask(dto.name(), null);
 
-        try {
-            TaskModel model = mapper.fromDtoToModel(dto);
-            checkDupeTask(dto.name(), null);
-            TaskModel savedTask = this.repository.save(model);
-            log.info("New task was saved successfully");
 
-            return mapper.fromModelToDto(savedTask);
-        } catch (Exception e) {
-            log.error("Couldn't create a new task", e);
-            throw new RuntimeException("Couldn't create a new task", e);
-        }
+        TaskModel model = mapper.fromDtoToModel(dto);
+        TaskModel savedTask = this.repository.save(model);
+        log.info("New task was saved successfully");
+
+        return mapper.fromModelToDto(savedTask);
     }
 
     @Transactional
     public TaskDto updateTask(long id, TaskDto dto) {
         log.info("Attempting to update the task with the id: {}", id);
 
-        try {
-            TaskModel model = getTask(id);
+        TaskModel model = getTask(id);
 
-            checkDupeTask(dto.name(), id);
-            mapper.updateModelFromDto(dto, model);
+        checkDupeTask(dto.name(), id);
+        mapper.updateModelFromDto(dto, model);
 
-            log.info("The task {} was updated successfully", id);
+        log.info("The task {} was updated successfully", id);
 
-            return mapper.fromModelToDto(model);
-        } catch (Exception e) {
-            log.error("Failed to update the task with the id {}", id);
-            throw new RuntimeException("Couldn't update the task " + id, e);
-        }
+        return mapper.fromModelToDto(model);
     }
 
     @Transactional
     public TaskDto deleteTask(long id) {
         log.info("Attempting to delete the task with the id {}", id);
 
-        try {
-            TaskModel model = getTask(id);
+        TaskModel model = getTask(id);
 
-            this.repository.delete(model);
-            log.info("The task with the id {} was deleted successfully", id);
-
-            return mapper.fromModelToDto(model);
-        } catch (Exception e) {
-            log.error("Failed to delete the task with the id {}", id);
-            throw new RuntimeException("Couldn't delete the task " + id, e);
-        }
+        this.repository.delete(model);
+        log.info("The task with the id {} was deleted successfully", id);
+        return mapper.fromModelToDto(model);
     }
 
 }
